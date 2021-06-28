d89a3e15fb07028a3eec1199831e6981cde19c31
Benjamin Golden <b.j.golden@outlook.com>
2021-06-28 13:19:16 -0500

    saved progress on comments

---
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+
+namespace TabloidMVC.Repositories
+{
+    public class CommentRepository : BaseRepository, ICommentRepository
+    {
+        public CommentRepository(IConfiguration config) :base(config) { }
+        public List<Comments> GetAllComments()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT * FROM Comment";
+
+                    var reader = cmd.ExecuteReader();
+
+                    var comments = new List<Comments>();
+
+                    while (reader.Read())
+                    {
+                        comments.Add(NewCommentFromUser(reader));
+                    }
+                    reader.Close();
+
+                    return comments;
+                }
+            }
+        }
+    }
+}

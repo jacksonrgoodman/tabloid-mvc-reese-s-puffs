d2152e3de3714b05212ffca784a6d7bcb79dc5bf
Micaela Colpi <micaelacolpi@gmail.com>
2021-06-29 14:58:53 -0500

    Startin on edit

--- TabloidMVC/Repositories/PostRepository.cs
+++ TabloidMVC/Repositories/PostRepository.cs
@@ -222,32 +222,39 @@ namespace TabloidMVC.Repositories
             }
         }
 
-        //public void UpdatePost (Post post)
-        //{
-        //    using (var conn = Connection)
-        //    {
-        //        conn.Open();
-        //        using (var cmd = conn.CreateCommand())
-        //        {
-        //            cmd.CommandText = @"
-        //                UPDATE Post (
-        //                    Title, Content, ImageLocation, CreateDateTime, PublishDateTime,
-        //                    IsApproved, CategoryId, UserProfileId )
-        //                OUTPUT INSERTED.ID
-        //                VALUES (
-        //                    @Title, @Content, @ImageLocation, @CreateDateTime, @PublishDateTime,
-        //                    @IsApproved, @CategoryId, @UserProfileId )";
-        //            cmd.Parameters.AddWithValue("@Title", post.Title);
-        //            cmd.Parameters.AddWithValue("@Content", post.Content);
-        //            cmd.Parameters.AddWithValue("@ImageLocation", DbUtils.ValueOrDBNull(post.ImageLocation));
-        //            cmd.Parameters.AddWithValue("@CreateDateTime", post.CreateDateTime);
-        //            cmd.Parameters.AddWithValue("@PublishDateTime", DbUtils.ValueOrDBNull(post.PublishDateTime));
-        //            cmd.Parameters.AddWithValue("@IsApproved", post.IsApproved);
-        //            cmd.Parameters.AddWithValue("@CategoryId", post.CategoryId);
-        //            cmd.Parameters.AddWithValue("@UserProfileId", post.UserProfileId);
-        //        }
-        //    }
-        //}
+        public void UpdatePost(Post post)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        UPDATE Post 
+                            SET
+                                Title = @title,
+                                Content = @content,
+                                ImageLocation = @imageLocation,
+                                CreateDateTime = @createDateTime,
+                                PublishDateTime = @publishDateTime,
+                                IsApproved = @isApproved,
+                                CategoryId = @categoryId,
+                                UserProfileId = @userProfileId
+                                WHERE ID = @id";
+                        
+                    cmd.Parameters.AddWithValue("@Title", post.Title);
+                    cmd.Parameters.AddWithValue("@Content", post.Content);
+                    cmd.Parameters.AddWithValue("@ImageLocation", DbUtils.ValueOrDBNull(post.ImageLocation));
+                    cmd.Parameters.AddWithValue("@CreateDateTime", post.CreateDateTime);
+                    cmd.Parameters.AddWithValue("@PublishDateTime", DbUtils.ValueOrDBNull(post.PublishDateTime));
+                    cmd.Parameters.AddWithValue("@IsApproved", post.IsApproved);
+                    cmd.Parameters.AddWithValue("@CategoryId", post.CategoryId);
+                    cmd.Parameters.AddWithValue("@UserProfileId", post.UserProfileId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
 
 
         private Post NewPostFromReader(SqlDataReader reader)

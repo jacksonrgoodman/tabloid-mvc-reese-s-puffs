4c8465d4062ab1c69e93c9c9e6a4a27fa8d54358
Benjamin Golden <b.j.golden@outlook.com>
2021-06-29 13:41:54 -0500

    users able to add a comment on a post

--- TabloidMVC/Repositories/CommentRepository.cs
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -61,17 +61,61 @@ namespace TabloidMVC.Repositories
                 conn.Open();
                 using (var cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = @"INSERT INTO Comment (PostId, Subject, Content, CreateDateTime, UserProfile.DisplayName) OUTPUT INSTERTED.ID VALUES (@PostId, @Subject, @Content, @CreateDateTime, @UserProfile.DisplayName)";
+                    cmd.CommandText = @"INSERT INTO Comment (PostId, Subject, Content, CreateDateTime, UserProfileId) OUTPUT INSERTED.ID VALUES (@PostId, @Subject, @Content, @CreateDateTime, @UserProfileId)";
                     cmd.Parameters.AddWithValue("@PostId", comment.PostId);
                     cmd.Parameters.AddWithValue("@Subject", comment.Subject);
                     cmd.Parameters.AddWithValue("@Content", comment.Content);
                     cmd.Parameters.AddWithValue("@CreateDateTime", comment.CreateDateTime);
-                    cmd.Parameters.AddWithValue("@UserProfile.DisplayName", comment.UserProfile.DisplayName);
+                    cmd.Parameters.AddWithValue("@UserProfileId", comment.UserProfileId);
 
                     comment.Id = (int)cmd.ExecuteScalar();
                 }
             }
         }
+
+
+
+        //public List<Comments> GetAll()
+        //{
+        //    using (var conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (var cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"Select up.DisplayName as AuthorName, c.Subject, c.Content,
+        //            c.CreateDateTime as Date, p.Title as Title  
+        //            from Comment c
+        //            left Join Post p on c.PostId = p.Id
+        //            left join UserProfile up on c.UserProfileId = up.Id
+        //            where p.id = @id
+        //            order by c.CreateDateTime desc;";
+
+        //            SqlDataReader reader = cmd.ExecuteReader();
+        //            List<Comments> comments = new List<Comments>();
+
+        //            while (reader.Read())
+        //            {
+        //                Comments comment = new Comments()
+        //                {
+        //                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+        //                    PostId = reader.GetInt32(reader.GetOrdinal("PostId")),
+
+        //                    Subject = reader.GetString(reader.GetOrdinal("Subject")),
+        //                    Content = reader.GetString(reader.GetOrdinal("Content")),
+        //                    CreateDateTime = reader.GetDateTime(reader.GetOrdinal("Date")),
+        //                    UserProfile = new UserProfile
+        //                    {
+        //                        DisplayName = reader.GetString(reader.GetOrdinal("AuthorName"))
+        //                    }
+        //                };
+        //                comments.Add(comment);
+        //            }
+        //            reader.Close();
+        //            return comments;
+
+        //        }
+        //    }
+        //}
 
     }
 

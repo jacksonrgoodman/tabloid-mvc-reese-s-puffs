f91c85fe64f1570a4128e24399cf2feeb80eac16
Traye <johnson.traye@yahoo.com>
2021-07-01 09:31:19 -0500

    merging main

---
+++ TabloidMVC/Repositories/SubscriptionRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class SubscriptionRepository : BaseRepository, ICategoryRepository
+    {
+        public SubscriptionRepository(IConfiguration config) : base(config) { }
+
+        public void CreateSubscription(Subscription subscription)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Subscription (SubscriberUserProfileId, )
+                        OUTPUT INSERTED.ID
+                        VALUES (@subscriptionUserProfileId)";
+
+                    cmd.Parameters.AddWithValue("@name", subscription.SubscriberUserProfileId);
+
+                    int id = (int)cmd.ExecuteScalar();
+
+                    subscription.Id = id;
+                }
+            }
+        }
+    }
+}

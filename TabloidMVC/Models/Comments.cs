e7515a9bc3d5484ae51d0a12ee9daffe03d17fbc
Benjamin Golden <b.j.golden@outlook.com>
2021-07-01 09:57:59 -0500

    saved progress on data annotations

--- TabloidMVC/Models/Comments.cs
+++ TabloidMVC/Models/Comments.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace TabloidMVC.Models
 {
